37b97799ab5dc596a4c5308eef7b522c9ce81417
Dilip Kumar <curiousdilip@gmail.com>
2022-12-05 19:53:38 +0530

    Dynamic Header Added

---
+++ src/data/data.js
@@ -0,0 +1,29 @@
+export const data = {
+  bgColor: "#81ffd9",
+  fontColor: "#333333",
+  title: {
+    name: "Dilip Kumar Maurya",
+    position: "left",
+    bgColor: "",
+    fontColor: "",
+    margin: "1rem",
+  },
+  menuDetails: {
+    margin: "1rem",
+    menuList: [
+      { name: "Home", order: 1 },
+      { name: "About", order: 2 },
+      { name: "Services", order: 3 },
+      { name: "Contact", order: 4 },
+      { name: "Privacy", order: 5 },
+      { name: "Privacy", order: 6 },
+      { name: "Privacy", order: 7 },
+      { name: "Privacy", order: 8 },
+      { name: "Privacy", order: 9 },
+      { name: "Privacy", order: 10 },
+      { name: "Privacy", order: 11 },
+      { name: "Privacy", order: 12 },
+      { name: "Privacy", order: 13 },
+    ],
+  },
+};
